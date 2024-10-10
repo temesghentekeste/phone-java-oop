69728fe997e56ca57cd3e1ab429e1bab0872d149
temesghentekeste <mail.temesghen.tekeste@gmail.com>
2024-10-10 17:01:55 +0300

    DefangIPaddr

--- src/main/java/org/develhope/advanced/java/codingchallenge/DefangIPaddr.java
+++ src/main/java/org/develhope/advanced/java/codingchallenge/DefangIPaddr.java
@@ -1,4 +1,14 @@
 package org.develhope.advanced.java.codingchallenge;
 
-public class IP {
+public class DefangIPaddr {
+    public static String defangIPaddr(String address) {
+
+        return address.replace(".", "[.]");
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println( defangIPaddr("1.1.1.1"));
+        System.out.println( defangIPaddr("255.100.50.0"));
+    }
 }
