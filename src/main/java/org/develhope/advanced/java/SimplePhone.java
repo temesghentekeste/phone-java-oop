e7b0ec07216b4551cc31f2cfbface446efb9e277
temesghentekeste <mail.temesghen.tekeste@gmail.com>
2024-10-10 16:00:09 +0300

    Implement basic Simplephone functionality

--- src/main/java/org/develhope/advanced/java/SimplePhone.java
+++ src/main/java/org/develhope/advanced/java/SimplePhone.java
@@ -1,20 +1,58 @@
 package org.develhope.advanced.java;
 
+import java.util.List;
+import java.util.Scanner;
+
 public abstract class SimplePhone implements Phone{
+    Scanner scanner = new Scanner(System.in);
+    List<String> callLogContacts;
+    List<String> textlLogContacts;
+    List<String> textlLogMessages;
+
+    public SimplePhone(List<String> callLogContacts, List<String> textlLogContacts, List<String> textlLogMessages) {
+        this.callLogContacts = callLogContacts;
+        this.textlLogContacts = textlLogContacts;
+        this.textlLogMessages = textlLogMessages;
+    }
+
     public void call() {
-        System.out.println(this.getClass().getSimpleName() + " is making a call");
+        System.out.print("Who do you want to call? ");
+        String contactName = scanner.nextLine().trim();
+        callLogContacts.add(contactName);
     }
 
     public void text() {
-        System.out.println(this.getClass().getSimpleName() + " is writing a text");
+        System.out.print("Who do you want to text? ");
+        String contactName = scanner.nextLine().trim();
+        System.out.print("Enter your message: ");
+        String message = scanner.nextLine().trim();
+        textlLogContacts.add(contactName);
+        textlLogMessages.add(contactName);
     }
 
     public void showCallHistory() {
+        if( callLogContacts.isEmpty()) {
+            System.out.println("Call log is empty.");
+            return;
+        }
+
         System.out.println(this.getClass().getSimpleName() + " is showing call history");
+        for (int i = 0; i < callLogContacts.size(); i++) {
+            System.out.println((i+1) + ". " + callLogContacts.get(i));
+        }
+
     }
 
     public void showTextHistory() {
-        System.out.println(this.getClass().getSimpleName() + " is showing text history");
+        if( textlLogContacts.isEmpty()) {
+            System.out.println("No recent messages available.");
+            return;
+        }
+
+        System.out.println(this.getClass().getSimpleName() + " is showing text message history");
+        for (int i = 0; i < textlLogContacts.size(); i++) {
+            System.out.println((i+1) + ". " + callLogContacts.get(i));
+        }
     }
 
 }
